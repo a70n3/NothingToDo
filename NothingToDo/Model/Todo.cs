d7a9ca55bf6411c57e688034d0cd6b2d3b21cfa4
a70n3 <prohibitous@gmail.com>
2021-08-28 15:48:36 +0800

    Add project files.

---
+++ NothingToDo/Model/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace NothingToDo.Model
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Task { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
